aae42a3f9981347c1c573ddad71daf92f1b3db94
thomasslai <lai.thomas@outlook.de>
2018-04-05 16:01:35 +0200

    page.php cleanup

--- page.php
+++ page.php
@@ -15,23 +15,23 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 
     if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
 ?>
-    
+<!--     
     <div class="col s12 offset-m2 m8">
         <div class="card">
             <div class="card-image">
                 <img src="<?php the_post_thumbnail_url( array(520, 390)); ?>">
             </div>
             <div class="card-content">
-            <span class="card-title with-image"><?php the_title(); ?></span>
-                <p><?php the_content(); ?></p>
-            </div>
+            <span class="card-title with-image"><?php the_title(); ?></span> -->
+            <?php the_content(); ?>
+            <!-- </div>
             <div class="card-action">
                 <span><i class="material-icons left">access_time</i><?php the_date('d.m.Y'); ?></span>
             </div>
         </div>
         <span><?php previous_post_link("%link", "%title", true); ?></span><br>
         <span><?php next_post_link("%link", "%title", true); ?></span>
-    </div>
+    </div> -->
 
 <?php
     }
