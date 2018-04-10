092fe206e37a27a6b6cccec9326ba0d1c34d4f9f
Bastian Krones <bkrones.uni@gmail.com>
2018-04-10 17:23:15 +0200

    BLah

--- partials/navheader.php
+++ partials/navheader.php
@@ -24,8 +24,8 @@ if ($category == 'po') {
 <nav>
   <div class="nav-wrapper">
   <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
-    <ul>
-      <li><a href="https://elearning.physik.uni-frankfurt.de"><i class="material-icons left hide-on-small-only">web</i>Elearning</a></li>
+    <ul class="test">
+      <!-- <li><a href="https://elearning.physik.uni-frankfurt.de"><i class="material-icons left hide-on-small-only">web</i>Elearning</a></li> -->
       <li><a href="#!" class="<?php echo $is_blog_active; ?>"><i class="material-icons left hide-on-small-only">message</i>Blog</a></li>
       <li><a href=<?php echo $about_link;?>><i class="material-icons left hide-on-small-only">people</i>About</a></li>
       <li><a href=<?php echo $contact_link;?>><i class="material-icons left hide-on-small-only">contact_mail</i>Contact</a></li>
