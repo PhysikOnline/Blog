9d370972894f7f7dca2672ecb4c557f947c36580
thomasslai <lai.thomas@outlook.de>
2018-04-17 13:44:07 +0200

    ID removed from post titles

--- category-physikonline.php
+++ category-physikonline.php
@@ -24,7 +24,8 @@ if (have_posts()): while (have_posts()): the_post();
 		                    <img src="<?php the_post_thumbnail_url(array(520, 390));?>">
 		                </div>
 		                <div class="card-content">
-		                <span class="card-title with-image"><?php the_title();?>(<?php the_ID();?>)</span>
+						<!-- (<?php the_ID();?>) -->
+		                <span class="card-title with-image"><?php the_title();?></span>
 		                    <p><?php the_excerpt();?></p>
 		                </div>
 		                <div class="card-action">
